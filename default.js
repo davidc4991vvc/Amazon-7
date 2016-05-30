9238d4626b5566404f5e38f3d1f2895e27eead20
schreyerpeter <pschreyer12321@aol.com>
2016-05-30 13:59:26 -0700

    Created objects for items #1

---
+++ default.js
@@ -0,0 +1,14 @@
+var items = [
+  {name: 'Samsung Flatscreen Television', price: 499.99, brand: 'Samsung'},
+  {name: 'Apple iPhone 6s', price: 399.99, brand: 'Apple'},
+  {name: 'Vizio Laptop', price: 199.99, brand: 'Vizio'},
+  {name: 'Apple MacBook Pro', price: 1299.99, brand: 'Apple'},
+  {name: 'Harry Potter: Chamber of Secrets', price: 12.99, author: 'J.K. Rowling'},
+  {name: 'How to Win Friends & Influence People', price: 8.99, author: 'Dale Carnegie'},
+  {name: 'A Universe From Nothing', price: 11.99, author: 'Lawrence M Krauss'},
+  {name: 'The Big Picture', price: 9.99, author: 'Sean Carroll'},
+  {name: 'Interstellar', price: 12.99},
+  {name: 'Deadpool', price: 11.99},
+  {name: 'Star Wars: The Force Awakens', price: 13.99},
+  {name: 'Ex Machina', price: 12.99},
+]
