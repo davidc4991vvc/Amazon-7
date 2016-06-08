0b783a9a13b66f4403a22ce044115d6173e3b46e
schreyerpeter <pschreyer12321@aol.com>
2016-06-08 16:05:00 -0700

    Refactoring cart.

--- default.js
+++ default.js
@@ -1,16 +1,16 @@
 var items = [
-  {id: 21, name: 'Samsung Television', price: 499.99, brand: 'Samsung', rating: 4, image: 'Pictures/samsung.jpg', description: 'Enjoy Full HD viewing and enriched colors on this Samsung HDTV. Its Smart TV features let you stream videos and music, surf the Internet, download apps and more. Plus, watch your TV entertainment on your mobile device or vice versa.', reviews:['-Looks great in my living room!']},
-  {id: 2, name: 'Apple iPhone 6s', price: 399.99, brand: 'Apple', rating: 4.5, image: 'Pictures/iphones.jpeg', description: 'The moment you use iPhone 6s, you know you’ve never felt anything like it. With a single press, 3D Touch lets you do more than ever before. Live Photos bring your memories to life in a powerfully vivid way. And that’s just the beginning. Take a deeper look at iPhone 6s, and you’ll find innovation on every level.', reviews:['-So many apps.']},
-  {id: 3, name: 'Vizio Laptop', price: 199.99, brand: 'Vizio', rating: 3, image: 'Pictures/vizio.jpeg', description: 'The remarkably powerful and beautifully portable VIZIO Notebook PC is packed with premium entertainment features, from Full HD display and premium audio to enhanced graphics, to let you work all day and play all night.', reviews:['-Such enhanced graphics.']},
-  {id: 4, name: 'Apple MacBook Pro', price: 1299.99, brand: 'Apple', rating: 4.5, image: 'Pictures/macbook.jpeg', description: 'Apple - MacBook Pro with Retina display (Latest Model) - 13.3" Display - 8GB Memory - 512GB Flash Storage - Silver', reviews:['-I went on the internet!']},
-  {id: 5, name: 'Harry Potter: Chamber of Secrets', price: 12.99, author: 'J.K. Rowling', rating: 5, image: 'Pictures/harry.jpg', description: 'Young wizard Harry Potter and his friends, Ron and Hermione, face new challenges during their second year at Hogwarts School of Witchcraft and Wizardry as they try to discover a dark force that is terrorizing the school.', reviews:["-<3 Harry"]},
-  {id: 6, name: 'How to Win Friends & Influence People', price: 8.99, author: 'Dale Carnegie', rating: 5, image: 'Pictures/winFriends.jpg', description: 'Written by Dale Carnegie and first published in 1936, it has sold over 30 million copies world-wide, and went on to be named #19 on Time Magazine\'s list of 100 most influential books in 2011.', reviews:['-Blew my mind.']},
-  {id: 7, name: 'A Universe From Nothing', price: 11.99, author: 'Lawrence M Krauss', rating: 3.5, image: 'Pictures/universe.jpeg', description: 'A Universe from Nothing: Why There Is Something Rather than Nothing is a book by physicist Lawrence M. Krauss, initially published on January 10, 2012 by Free Press.', reviews:['-Fascinating book!']},
-  {id: 8, name: 'The Big Picture', price: 9.99, author: 'Sean Carroll', rating: 4, image: 'Pictures/bigPicture.jpeg', description: 'Already internationally acclaimed for his elegant, lucid writing on the most challenging notions in modern physics.', reviews:['-This book is interesting!']},
-  {id: 9, name: 'Interstellar', price: 12.99, rating: 2.5, image: 'Pictures/interstellar.jpeg', description: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\s population to a new home via a wormhole.', reviews:['-Made me hungry for corn.']},
-  {id: 10, name: 'Deadpool', price: 11.99, rating: 4, image: 'Pictures/deadpool.jpg', description: 'Wade Wilson (Ryan Reynolds) is a former Special Forces operative who now works as a mercenary. His world comes crashing down when evil scientist Ajax (Ed Skrein) tortures, disfigures and transforms him into Deadpool. The rogue experiment leaves Deadpool with accelerated healing powers and a twisted sense of humor.', reviews:['-Entertaining for sure.']},
-  {id: 11, name: 'Star Wars: The Force Awakens', price: 13.99, rating: 5, image: 'Pictures/starWars.jpg', description: 'Thirty years after the defeat of the Galactic Empire, the galaxy faces a new threat from the evil Kylo Ren (Adam Driver) and the First Order. When a defector named Finn crash-lands on a desert planet, he meets Rey (Daisy Ridley), a tough scavenger whose droid contains a top-secret map.', reviews:['-Excited for the next ones!']},
-  {id: 12, name: 'Ex Machina', price: 12.99, rating: 3.5, image: 'Pictures/exMachina.jpg', description:'Caleb Smith (Domhnall Gleeson) a programmer at a huge Internet company, wins a contest that enables him to spend a week at the private estate of Nathan Bateman (Oscar Isaac), his firm\'s brilliant CEO. When he arrives, Caleb learns that he has been chosen to be the human component in a Turing test to determine the capabilities and consciousness of Ava (Alicia Vikander), a beautiful robot.', reviews:['-Skynet online...']},
+  {id: 21, name: 'Samsung Television', price: 499.99, brand: 'Samsung', rating: 4, image: 'Pictures/samsung.jpg', description: 'Enjoy Full HD viewing and enriched colors on this Samsung HDTV. Its Smart TV features let you stream videos and music, surf the Internet, download apps and more. Plus, watch your TV entertainment on your mobile device or vice versa.', reviews:['-Looks great in my living room!'], quantity: 0},
+  {id: 2, name: 'Apple iPhone 6s', price: 399.99, brand: 'Apple', rating: 4.5, image: 'Pictures/iphones.jpeg', description: 'The moment you use iPhone 6s, you know you’ve never felt anything like it. With a single press, 3D Touch lets you do more than ever before. Live Photos bring your memories to life in a powerfully vivid way. And that’s just the beginning. Take a deeper look at iPhone 6s, and you’ll find innovation on every level.', reviews:['-So many apps.'], quantity: 0},
+  {id: 3, name: 'Vizio Laptop', price: 199.99, brand: 'Vizio', rating: 3, image: 'Pictures/vizio.jpeg', description: 'The remarkably powerful and beautifully portable VIZIO Notebook PC is packed with premium entertainment features, from Full HD display and premium audio to enhanced graphics, to let you work all day and play all night.', reviews:['-Such enhanced graphics.'], quantity: 0},
+  {id: 4, name: 'Apple MacBook Pro', price: 1299.99, brand: 'Apple', rating: 4.5, image: 'Pictures/macbook.jpeg', description: 'Apple - MacBook Pro with Retina display (Latest Model) - 13.3" Display - 8GB Memory - 512GB Flash Storage - Silver', reviews:['-I went on the internet!'], quantity: 0},
+  {id: 5, name: 'Harry Potter: Chamber of Secrets', price: 12.99, author: 'J.K. Rowling', rating: 5, image: 'Pictures/harry.jpg', description: 'Young wizard Harry Potter and his friends, Ron and Hermione, face new challenges during their second year at Hogwarts School of Witchcraft and Wizardry as they try to discover a dark force that is terrorizing the school.', reviews:["-<3 Harry"], quantity: 0},
+  {id: 6, name: 'How to Win Friends & Influence People', price: 8.99, author: 'Dale Carnegie', rating: 5, image: 'Pictures/winFriends.jpg', description: 'Written by Dale Carnegie and first published in 1936, it has sold over 30 million copies world-wide, and went on to be named #19 on Time Magazine\'s list of 100 most influential books in 2011.', reviews:['-Blew my mind.'], quantity: 0},
+  {id: 7, name: 'A Universe From Nothing', price: 11.99, author: 'Lawrence M Krauss', rating: 3.5, image: 'Pictures/universe.jpeg', description: 'A Universe from Nothing: Why There Is Something Rather than Nothing is a book by physicist Lawrence M. Krauss, initially published on January 10, 2012 by Free Press.', reviews:['-Fascinating book!'], quantity: 0},
+  {id: 8, name: 'The Big Picture', price: 9.99, author: 'Sean Carroll', rating: 4, image: 'Pictures/bigPicture.jpeg', description: 'Already internationally acclaimed for his elegant, lucid writing on the most challenging notions in modern physics.', reviews:['-This book is interesting!'], quantity: 0},
+  {id: 9, name: 'Interstellar', price: 12.99, rating: 2.5, image: 'Pictures/interstellar.jpeg', description: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\s population to a new home via a wormhole.', reviews:['-Made me hungry for corn.'], quantity: 0},
+  {id: 10, name: 'Deadpool', price: 11.99, rating: 4, image: 'Pictures/deadpool.jpg', description: 'Wade Wilson (Ryan Reynolds) is a former Special Forces operative who now works as a mercenary. His world comes crashing down when evil scientist Ajax (Ed Skrein) tortures, disfigures and transforms him into Deadpool. The rogue experiment leaves Deadpool with accelerated healing powers and a twisted sense of humor.', reviews:['-Entertaining for sure.'], quantity: 0},
+  {id: 11, name: 'Star Wars: The Force Awakens', price: 13.99, rating: 5, image: 'Pictures/starWars.jpg', description: 'Thirty years after the defeat of the Galactic Empire, the galaxy faces a new threat from the evil Kylo Ren (Adam Driver) and the First Order. When a defector named Finn crash-lands on a desert planet, he meets Rey (Daisy Ridley), a tough scavenger whose droid contains a top-secret map.', reviews:['-Excited for the next ones!'], quantity: 0},
+  {id: 12, name: 'Ex Machina', price: 12.99, rating: 3.5, image: 'Pictures/exMachina.jpg', description:'Caleb Smith (Domhnall Gleeson) a programmer at a huge Internet company, wins a contest that enables him to spend a week at the private estate of Nathan Bateman (Oscar Isaac), his firm\'s brilliant CEO. When he arrives, Caleb learns that he has been chosen to be the human component in a Turing test to determine the capabilities and consciousness of Ava (Alicia Vikander), a beautiful robot.', reviews:['-Skynet online...'], quantity: 0},
 ]
 
 var users = [
@@ -99,28 +99,61 @@ returnHomeButton.addEventListener('click', function(theEvent){
 });
 
 var cart = {
-  items: []
+  items: [],
+  quantity: []
 }
 var orderHistory = {
-  purchasedItems: []
+  purchasedItems: [],
+  quantity: []
 }
 
 var results = document.getElementById('results');
 results.addEventListener('click', function(theEvent){
   theId = theEvent.target.getAttribute('id');
-  var counter = document.getElementById('itemCounter');
-  var count = parseInt(counter.textContent);
-  if(theId !== null && theId !== 'results'){
-    count++;
-  }
-  counter.textContent = count;
 
+  // var counter = document.getElementById('itemCounter');
+  // var count = parseInt(counter.textContent);
+  //
+  // if(theId !== null && theId !== 'results'){
+  //   count++;
+  // }
+
+  // counter.textContent = count;
+
+  console.log('Adding something to the cart now.')
+
+  // Go through all the items.
   items.forEach(function(item){
     if (item.id == theId) {
-      cart.items.push(item);
+      addToCart(item, cart);
     }
   })
 });
+
+// Add the item to the cart.
+function addToCart(item, cart) {
+  // Check if the item is in the cart already.
+  for (var i = 0; i < cart.items.length; i++) {
+
+    // If the item that was clicked is already in the cart.
+    console.log(theId, cart.items[i].id);
+    if (theId == cart.items[i].id) {
+      console.log('We found the item, it has a quantity of ' + cart.items[i].quantity);
+      cart.items[i].quantity++;
+    } else {
+      console.log('We did not find it in the cart. Adding it with a quantity of 1');
+      // If the item isn't in the cart. Add it.
+      cart.items.push(item);
+    }
+  }
+
+  // If cart is empty.
+  if (cart.items.length === 0) {
+    item.quantity = 1;
+    cart.items.push(item);
+  }
+}
+
 
 var subTotal = document.getElementById('subTotal');
 subTotal.textContent = "Your cart is empty.";
@@ -136,6 +169,7 @@ function subTotal(cart){
 var sub = 0;
 var itemCount = 0;
 var viewCart = document.getElementById('cartIcon');
+var itemQuantity = 0;
 viewCart.addEventListener('click',function(theEvent){
   itemCount=0;
   var pageOne = document.getElementById('firstPage');
@@ -145,34 +179,41 @@ viewCart.addEventListener('click',function(theEvent){
   for (var i = 0; i < cart.items.length; i++) {
     var theRow = document.createElement('div');
     theRow.setAttribute('class', 'row cartRow');
+    console.log(cart.items[0].name);
+    if(cart.items.name != cart.items[i].name){
+      var theImage = document.createElement('div');
+      theImage.className = 'col-xs-6 col-offset-xs-1 itemImg';
+      var image = document.createElement('img');
+      image.src = cart.items[i].image;
+      image.setAttribute('class', 'cartItems')
+      theImage.appendChild(image);
 
-    var theImage = document.createElement('div');
-    theImage.className = 'col-xs-6 col-offset-xs-1 itemImg';
-    var image = document.createElement('img');
-    image.src = cart.items[i].image;
-    image.setAttribute('class', 'cartItems')
-    theImage.appendChild(image);
+      var theItem = document.createElement('div');
+      theItem.className = "col-xs-3 text-center";
+      var itemName = document.createElement('h4');
+      itemName.textContent = cart.items[i].name;
+      theItem.appendChild(itemName);
 
-    var theItem = document.createElement('div');
-    theItem.className = "col-xs-3 text-center";
-    var itemName = document.createElement('h4');
-    itemName.textContent = cart.items[i].name;
-    theItem.appendChild(itemName);
+      var thePrice = document.createElement('h4');
+      thePrice.textContent = "$" + cart.items[i].price;
+      thePrice.setAttribute('class','itemPrice col-xs-3 text-center');
 
-    var thePrice = document.createElement('h4');
-    thePrice.textContent = "$" + cart.items[i].price;
-    thePrice.setAttribute('class','itemPrice col-xs-3 text-center');
+      var theDescription = document.createElement('p');
+      theDescription.textContent = cart.items[i].description;
+      theDescription.setAttribute('class', 'itemDescription');
+      theItem.appendChild(theDescription);
 
-    var theDescription = document.createElement('p');
-    theDescription.textContent = cart.items[i].description;
-    theDescription.setAttribute('class', 'itemDescription');
-    theItem.appendChild(theDescription);
+      var theSpace = document.getElementById('newPage');
+      theRow.appendChild(theImage);
+      theRow.appendChild(theItem);
+      theRow.appendChild(thePrice);
+      theSpace.appendChild(theRow);
 
-    var theSpace = document.getElementById('newPage');
-    theRow.appendChild(theImage);
-    theRow.appendChild(theItem);
-    theRow.appendChild(thePrice);
-    theSpace.appendChild(theRow);
+      itemQuantity++;
+    }
+    else{
+      itemQuantity++;
+    }
 
     itemCount++;
     function itemCountMessage(itemCount){
@@ -370,6 +411,7 @@ allItems.addEventListener('click', function(theEvent){
       addToCart.textContent = "Add to Cart";
       addToCart.addEventListener('click', function(theEvent){
         cart.items.push(items[theId]);
+        items[theId].quantity += 1;
         var counter = document.getElementById('itemCounter');
         var count = parseInt(counter.textContent);
         count++;
@@ -403,6 +445,7 @@ returnBackToHome.addEventListener('click', function(theEvent){
   document.getElementById('results');
   var pageOne = document.getElementById('firstPage');
   var historyPage = document.getElementById('historyPage');
+  clear(purchasedItemsList);
   toggle(pageOne, historyPage);
 });
 
@@ -412,15 +455,13 @@ viewHistory.addEventListener('click', function(theEvent){
   var purchasedItemsList = document.getElementById('purchasedItemsList');
   var historyPage = document.getElementById('historyPage');
   var pageOne = document.getElementById('firstPage');
-  console.log(orderHistory);
   toggle(historyPage, pageOne);
 
-  var historyRow = document.createElement('div');
-  historyRow.setAttribute('class','row historyRow');
   var purchasedList = document.createElement('ul');
   for (var i = 0; i < orderHistory.purchasedItems.length; i++) {
+    var quantity = 0;
     var purchasedItem = document.createElement('li');
-    purchasedItem.textContent = orderHistory.purchasedItems[i];
+    purchasedItem.textContent = orderHistory.purchasedItems[i] +" Quantity: " +quantity;
     purchasedList.appendChild(purchasedItem);
     purchasedItemsList.appendChild(purchasedList);
   }
